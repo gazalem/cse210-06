1c1411feced604080364738c43386ae1ad3c2191
antmorera <ant.morera.lb@gmail.com>
2022-12-09 23:57:20 -0700

    updated the racket.py and the import .py files

--- pingPong/game/casting/actor.py
+++ pingPong/game/casting/actor.py
@@ -3,20 +3,27 @@ from game.casting.text import Text
 
 class Actor:
     """A thing that participates in the game."""
-    
-    def __init__(self, debug = False):
+
+    def __init__(self, debug=False):
         """Constructs a new Actor using the given group and id.
-        
+
         Args:
             group: A string containing the actor's group name.
             id: A number that uniquely identifies the actor within the group.
         """
+        self._idx = 0
         self._debug = debug
-        
+
     def is_debug(self):
         """Whether or not the actor is being debugged.
-        
+
         Returns:
             True if the actor is being debugged; False if otherwise.
         """
         return self._debug
+
+    def set_idx(self, idx):
+        self._idx = idx
+
+    def get_idx(self):
+        return self._idx
